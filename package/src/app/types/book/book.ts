d14c5bc90ba986734d7c72d14a5b32ea1de7575a
Laura <laura.erhan@isa.utm.md>
2026-02-15 14:41:06 +0200

    created type book and mockdata

---
+++ package/src/app/types/book/book.ts
@@ -0,0 +1,14 @@
+type Book = {
+    id: number;
+    title: string;
+    author: string;
+    genre: string;
+    condition: string;
+    cover_image: string;
+    owner_name: string;
+    owner_image: string;
+    availableForSwap: boolean;
+    featured: boolean;
+    rating: number;
+    publishYear: number;
+};
