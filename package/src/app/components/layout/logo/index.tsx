9a390f46c11c21c106beb7208b43c540131d4fbb
Patricia <patriciapopa3@gmail.com>
2026-02-15 22:42:08 +0200

    updated logo

--- package/src/app/components/layout/logo/index.tsx
+++ package/src/app/components/layout/logo/index.tsx
@@ -1,15 +1,12 @@
-import Image from "next/image"
 import Link from "next/link"
 
 const Logo = () => {
-  return (
-    <>
-        <Link href="/">
-            <Image src={"/images/logo/logo.svg"} alt="logo" width={135} height={35} className="block dark:hidden"/>
-            <Image src={"/images/logo/logo-white.svg"} alt="logo" width={135} height={35} className="hidden dark:block"/>
+    return (
+        <Link href="/" className="text-2xl font-bold inline-flex items-center">
+            <span className="bg-black dark:bg-white text-white dark:text-black px-3 py-1.5">READ</span>
+            <span className="text-black dark:text-white ml-1">& SWAP</span>
         </Link>
-    </>
-  )
+    )
 }
 
 export default Logo
